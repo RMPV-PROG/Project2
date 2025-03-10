9f43244b418868b1091f8c8f2992ed1ce14f2830
Janis <test@gmail.com>
2025-03-10 11:16:36 +0200

    add DB

--- index.php
+++ index.php
@@ -4,5 +4,25 @@ require "functions.php";
 
 require 'router.php';
 
+require 'Database.php';
+
+$config = require 'config.php';
+
+$db = new Database($config['database']);
+
+// $id = $_GET['id'];
+
+// http://localhost:8080/contacts?id=1;drop%20table%20users - drop table users
+
+// $sql = "SELECT * FROM posts WHERE id = :id";  //->fetch()
+// // $sql = 'SELECT * FROM posts'; //->fetchAll()
+
+// $posts = $db->query($sql, [':id' => $id])->fetchAll();
+
+// print_r($posts);
+
+
+
+
 
 
